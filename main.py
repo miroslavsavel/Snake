ee56880f485912bd9155709a59ff5069dfe9c4b2
Bytesabre <miroslav.savel@gmail.com>
2022-03-31 23:56:15 +0200

    place fruit into the grid

--- main.py
+++ main.py
@@ -1,16 +1,33 @@
 import sys
 import pygame
-
+from pygame.math import Vector2
 # https://www.youtube.com/watch?v=QFvqStqPCRU
-pygame.init()
-screen = pygame.display.set_mode((400, 500))
-clock = pygame.time.Clock()
 
+class Fruit:
+    def __init__(self):
+        self.x = 0
+        self.y = 1
+        self.pos = Vector2(self.x, self.y)
+
+    def draw_fruit(self):
+        fruit_rect = pygame.Rect(self.pos.x * cell_size, self.pos.y * cell_size,cell_size,cell_size)
+        pygame.draw.rect(screen, (126,166,114), fruit_rect)
+
+
+
+
+pygame.init()
+cell_size = 40
+cell_number = 20
+screen = pygame.display.set_mode((cell_number * cell_size, cell_number * cell_size))
+clock = pygame.time.Clock()
+fruit = Fruit()
 while True:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             pygame.quit()
             sys.exit()
     screen.fill((175,215,70))
+    fruit.draw_fruit()
     pygame.display.update()
     clock.tick(60)
