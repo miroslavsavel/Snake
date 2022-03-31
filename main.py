6ea313b4567f58182c27642ca6f6e72a0635c84c
Bytesabre <miroslav.savel@gmail.com>
2022-04-01 00:11:36 +0200

    draw snake into grid

--- main.py
+++ main.py
@@ -1,20 +1,29 @@
 import sys
 import pygame
 from pygame.math import Vector2
+import random
 # https://www.youtube.com/watch?v=QFvqStqPCRU
 
 class Fruit:
     def __init__(self):
-        self.x = 0
-        self.y = 1
+        self.x = random.randint(0, cell_number-1)
+        self.y = random.randint(0, cell_number-1)
         self.pos = Vector2(self.x, self.y)
 
     def draw_fruit(self):
         fruit_rect = pygame.Rect(self.pos.x * cell_size, self.pos.y * cell_size,cell_size,cell_size)
         pygame.draw.rect(screen, (126,166,114), fruit_rect)
 
+class Snake:
+    def __init__(self):
+        self.body = [Vector2(5,10),Vector2(6,10),Vector2(7,10)]
 
-
+    def draw_snake(self):
+        for block in self.body:
+            # create a rect
+            block_rect = pygame.Rect(block.x * cell_size, block.y * cell_size, cell_size, cell_size)
+            # draw the rectangle
+            pygame.draw.rect(screen,(180,126,114),block_rect)
 
 pygame.init()
 cell_size = 40
@@ -22,6 +31,7 @@ cell_number = 20
 screen = pygame.display.set_mode((cell_number * cell_size, cell_number * cell_size))
 clock = pygame.time.Clock()
 fruit = Fruit()
+snake = Snake()
 while True:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
@@ -29,5 +39,6 @@ while True:
             sys.exit()
     screen.fill((175,215,70))
     fruit.draw_fruit()
+    snake.draw_snake()
     pygame.display.update()
     clock.tick(60)
